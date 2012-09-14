9ecd5114a642f6ed4f427ed0e8f9e45c48aa6009
jdfdesign <regis@joufdesign.com>
2012-09-14 17:26:02 -0400

    bump

--- db/migrate/20120830105142_create_languages_table.gko_core.rb
+++ db/migrate/20120830105142_create_languages_table.gko_core.rb
@@ -6,13 +6,29 @@ class CreateLanguagesTable < ActiveRecord::Migration
         t.references :site
         t.string :name
         t.string :code
+        t.integer :position, :default => 1
         t.string :presentation
         t.boolean :public, :default => false
         t.boolean :default, :default => false
         t.timestamps
       end
       add_index :languages, :site_id
+      add_index :languages, [:site_id, :position]
+      
       add_column :sites, :languages_count, :integer, :default => 0
+      
+      Site.all.each do |site|
+        site.translated_locales.each_with_index do |locale, index|
+          site.languages.create!({
+            :name => I18n.t(:"locales.long.#{locale.to_s}"),
+            :code => locale.to_s,
+            :presentation => I18n.t(:"locales.long.#{locale.to_s}"),
+            :public => true,
+            :position => index + 1,
+            :default => (index == 0)
+          })
+        end
+      end
     end
 
   end
@@ -20,6 +36,7 @@ class CreateLanguagesTable < ActiveRecord::Migration
   def down
     remove_column :sites, :languages_count
     remove_index :languages, :site_id
+    remove_index :languages, [:site_id, :position]
     drop_table :languages
   end
 end
--- db/migrate/20120905144723_remove_mail_methods_table.gko_inquiries.rb
+++ db/migrate/20120905144723_remove_mail_methods_table.gko_inquiries.rb
@@ -2,20 +2,19 @@
 class RemoveMailMethodsTable < ActiveRecord::Migration
   def up
     if table_exists?(:mail_methods)
-      add_column :sites, :mailer_settings, :text unless column_exists?(:sites, :mailer_settings)
       pref_keys = ['mail_host', 'mail_domain', 'mail_port', 'mail_auth_type', 'smtp_username', 'smtp_password', 'delivery_method', 'secure_connection_type', 'mails_from', 'mail_bcc']
-      mail_methods = connection.execute("SELECT*FROM mail_methods WHERE environment='production'")
-      mail_methods.each do |mm|
-        site = Site.find(mm.site_id)
-        Site.current = site
+      result = connection.execute("SELECT id, site_id FROM mail_methods WHERE environment='production'")
+      result.each do |mm|
+        Site.current = site = Site.find(mm[1])
         pref_keys.each do |k|
-          if p = Preference.find_by_key("mail_method/#{k}/#{mm.id}")
-            site.mailer_settings[k] = p.value
+          if p = Preference.find_by_key("mail_method/#{k}/#{mm[0]}")
+            site[k.to_sym] = p.value
             p.destroy
           end
         end
         site.save!
       end
+      drop_table :mail_methods
     end
   end
 end
