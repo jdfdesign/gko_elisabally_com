82b3c3f9bd318f3dd9dd0861458deb05631ecb5c
regis bruggheman <regis@joufdesign.com>
2013-04-09 18:51:16 +0200

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,3 +1,14 @@
+//= require jquery
+//= require jquery_ujs
+//= require gko/public/jquery.grid.responsive.js
+//= require twitter/bootstrap/transition.js
+//= require twitter/bootstrap/alert.js
+//= require twitter/bootstrap/button.js
+//= require twitter/bootstrap/carousel.js
+//= require twitter/bootstrap/collapse.js
+//= require twitter/bootstrap/tab.js
+//= require twitter/bootstrap/dropdown.js
+//= require twitter/bootstrap/modal.js
 //= require gko/gko.galleria
 
 $(document).ready(function() {
